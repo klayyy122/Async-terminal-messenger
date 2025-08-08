1d0e19bff39cec18fd6017df4a34428e07f21550
Kasatk1n1A <kasatkin.ia@edu.spbstu.ru>
2025-08-08 19:52:27 +0300

    Общая либа в include там файл в который либы общие писать типа vector boost thread и другие

---
+++ include/some_libs.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+#include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
