4a44f9efa1a1b512f304b2670ae36710eee14547
Kasatk1n1A <kasatkin.ia@edu.spbstu.ru>
2025-08-08 19:29:49 +0300

    Deleted lib from repos

---
+++ src/client/client.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "./../../include/some_libs"
+
+class Client
+{
+
+};
--- src/test.cpp
+++ src/test.cpp
@@ -10,15 +10,67 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+
+#include <thread>
+#include <chrono>
+
+std::vector<char> vBuffer(1024);
+
+void GrabSomeData(boost::asio::ip::tcp::socket& socket)
+{
+  socket.async_read_some(boost::asio::buffer(vBuffer.data(), vBuffer.size()), 
+    [&](std::error_code ec, std::size_t lenght)
+    {
+      if (!ec)
+      {
+        std::cout << "\n\nRead " << lenght << " bytes\n\n";
+
+        for (int i = 0; i < lenght; i++)
+          std::cout << vBuffer[i];
+
+        GrabSomeData(socket);
+      }
+    });
+}
 
 int main()
 {
-  boost::asio::io_context io;
+  boost::system::error_code ec;
+  
+  boost::asio::io_context context;
 
-  boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
-  t.wait();
+  auto idleWork = boost::asio::make_work_guard(context);
+  
+  std::thread thrContext = std::thread([&](){ context.run(); });
+  
+  boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::make_address("51.38.81.49", ec), 80);
+  
+  boost::asio::ip::tcp::socket socket(context);
 
-  std::cout << "Hello, world!" << std::endl;
+  socket.connect(endpoint, ec);
+
+  if (!ec)
+    std::cout << "Connected!" << std::endl;
+  else
+    std::cout << "Failed to connect:\n" << ec.message() << std::endl;
+
+  if (socket.is_open())
+  {
+    GrabSomeData(socket); 
+
+    std::string sRequest = 
+      "GET /index.html HTTP/1.1\r\n"
+      "Host: example.com\r\n"
+      "Connection: close\r\n\r\n";
+
+    socket.write_some(boost::asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  }
+
+  system("pause");
 
   return 0;
 }
